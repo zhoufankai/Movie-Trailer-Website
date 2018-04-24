05fd948d3df305c41a19a3105ea944598ae0c684
Blaise <zfk2wind@gmail.com>
2018-04-24 08:16:38 +0800

    Update media.py

--- media.py
+++ media.py
@@ -1,9 +1,21 @@
 
 
 class Movie():
-    """ The class provids storing movie information """
+    """ The class provids storing movie information 
+    Args:        
+        title(str): movie trailer's title.
+        poster_image_url(str): the poster image's url.
+        trailer_youtube_url(str): the trailer's url on youtube.
+    Attributes:
+        title(str): movie trailer's title.
+        poster_image_url(str): the poster image's url.
+        trailer_youtube_url(str): the trailer's url on youtube.
+    """
     
     def __init__(self, title, poster_image_url, trailer_youtube_url):
+        """
+        
+        """
         self.title = title
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
